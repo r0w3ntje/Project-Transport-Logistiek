4a3fad1d5b518bb737fd751eb3d4b52e70fc6cdf
Rowen <r0w3nkuiper@hotmail.com>
2018-11-15 12:59:08 +0100

    Upgrade system very WIP

---
+++ Unity/Transport en Logistiek/Assets/Sandbox/Rowen/UpgradeSystem/Scripts/UpgradeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Systems.Singleton;
+using UnityEngine;
+
+namespace Systems.UpgradeSystem
+{
+    public enum MenuAction
+    {
+        Open, Close
+    }
+
+    public class UpgradeManager : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void UpgradeMenu(MenuAction _menuAction)
+        {
+
+        }
+    }
+}
