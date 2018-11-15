dcc6a27b1e24fa184bf72e7163b5bfda6115885a
Rowen <r0w3nkuiper@hotmail.com>
2018-11-15 12:17:47 +0100

    - player data (euro currency and trash collected)

---
+++ Unity/Transport en Logistiek/Assets/Content/Scripts/Data/Player/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Systems.PointSystem;
+using Systems.Singleton;
+
+public class PlayerData : Singleton<PlayerData>
+{
+    public float euro;
+    public int trash;
+
+    private void Start()
+    {
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        euro = PointSystem.Data(Action.Load, "euro", euro);
+        trash = PointSystem.Data(Action.Load, "trash", trash);
+
+        SaveData();
+    }
+
+    private void SaveData()
+    {
+        euro = PointSystem.Data(Action.Save, "euro", euro);
+        trash = PointSystem.Data(Action.Save, "trash", trash);
+    }
+}
