abfdb49e6453aa5b81f44efa510a5674ed45ceb5
Rowen <r0w3nkuiper@hotmail.com>
2018-11-27 11:32:37 +0100

    - Unit prefab & script & materials - scene save

--- Unity/Transport en Logistiek/Assets/Content/Scripts/Unit/Unit.cs
+++ Unity/Transport en Logistiek/Assets/Content/Scripts/Unit/Unit.cs
@@ -5,4 +5,28 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public UnitEnum UnitType;
+
+    [SerializeField] private Material iron, food;
+
+    private Renderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        var mats = meshRenderer.materials;
+
+        switch (UnitType)
+        {
+            case UnitEnum.Iron:
+                mats[0] = iron;
+                break;
+
+            case UnitEnum.Food:
+                mats[0] = food;
+                break;
+        }
+
+        meshRenderer.materials = mats;
+    }
 }
