0ec48aaa48e62e3329030f28043704f73109e67c
Tom Elgersma <tom.elgersma@hotmail.com>
2018-12-06 12:12:01 +0100

    GDD Changes and Fuel Meter Added
    small changes to the GDD and added the fuel meter in my sandbox folder

---
+++ Unity/Transport en Logistiek/Assets/Sandbox/Tom/Scripts/FuelBar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBar : MonoBehaviour
+{
+    public float fuelInt;     //the int for the amount the fuel will go down
+
+    private float time;
+    private Image fuelImage;
+
+    void Start()
+    {
+        fuelImage = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= 0.01f)
+        {
+            fuelImage.fillAmount = fuelImage.fillAmount - FuelCalc(fuelInt);
+            time = 0;
+            fuelImage.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+        }
+    }
+
+    public float FuelCalc(float newInt)
+    {
+        float fuel = fuelInt / 100000;
+        return fuel;
+    }
+}
