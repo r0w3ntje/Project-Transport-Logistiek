b1818ee800c421a919198f75c04af3739339be7c
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-07 12:08:53 +0100

    - new camera movement - moved scripts in project folder

---
+++ Unity/Transport en Logistiek/Assets/Content/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Factory
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraHolder;
+        [SerializeField] private new Transform camera;
+
+        [SerializeField] private float fixedHeight;
+
+        [SerializeField] private float movementSpeed;
+        [SerializeField] private float dragSpeed;
+
+        private bool isRotating;
+
+        private Vector3 mouseOrigin;
+        private Vector3 oldMousePos;
+
+        private Vector3 rotationPosition;
+        private Vector3 otherRotation;
+
+        private void Update()
+        {
+            Move();
+            Rotate();
+        }
+
+        private void Move()
+        {
+            cameraHolder.position += cameraHolder.forward * movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+            cameraHolder.position += cameraHolder.right * movementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        }
+
+        private Vector3 pos;
+        private void Rotate()
+        {
+            if (Input.GetMouseButtonDown(2))
+            {
+                mouseOrigin = Input.mousePosition;
+                isRotating = true;
+            }
+
+            if (Input.GetMouseButtonUp(2))
+            {
+                isRotating = false;
+            }
+
+            if (isRotating)
+            {
+                pos = Camera.main.ScreenToViewportPoint(mouseOrigin - Input.mousePosition);
+                cameraHolder.eulerAngles = new Vector3(0f, pos.x * dragSpeed, 0f) + otherRotation;
+            }
+            else otherRotation = cameraHolder.eulerAngles;
+        }
+    }
+}
