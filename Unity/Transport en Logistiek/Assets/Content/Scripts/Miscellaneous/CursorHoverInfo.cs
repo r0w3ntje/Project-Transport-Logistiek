8a6ee5877ed993d162f5b252684216cad2f43046
r0w3ntje <r0w3nkuiper@hotmail.com>
2018-12-14 10:28:11 +0100

    cursor info hover script

---
+++ Unity/Transport en Logistiek/Assets/Content/Scripts/Miscellaneous/CursorHoverInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Systems.Singleton;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorHoverInfo : Singleton<CursorHoverInfo>
+{
+    public Text hoverText;
+
+    private void Update()
+    {
+        if (hoverText.enabled)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+}
